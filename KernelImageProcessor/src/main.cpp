f99d85b867308d9d63b6654e20bcddccbcaf42b3
Geeoon Chung <geeoon13@gmail.com>
2021-05-01 12:30:38 -0500

    Kernel implimented, though gauss blur appears darker than input image; however, identity matrix works as it should.

--- KernelImageProcessor/src/main.cpp
+++ KernelImageProcessor/src/main.cpp
@@ -1,13 +1,12 @@
-#include "CImg.h"
 #include "png++/png.hpp"
 #include "math/Matrix.h"
 
 int main() {
 
 	png::image< png::rgb_pixel_16 > inputImage{ "images/source/liberty.png" };
-	Matrix< png::uint_16 > inputMatrixR{ inputImage.get_height(), inputImage.get_width() };
-	Matrix< png::uint_16 > inputMatrixG{ inputImage.get_height(), inputImage.get_width() };
-	Matrix< png::uint_16 > inputMatrixB{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > inputMatrixR{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > inputMatrixG{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > inputMatrixB{ inputImage.get_height(), inputImage.get_width() };
 
 	// load image to matrix for maniupulation
 	for (unsigned int y = 0; y < inputImage.get_height(); y++) {
@@ -18,17 +17,49 @@ int main() {
 		}
 	}
 
-	Matrix< png::uint_16 > outputMatrixR{ inputImage.get_height(), inputImage.get_width() };
-	Matrix< png::uint_16 > outputMatrixG{ inputImage.get_height(), inputImage.get_width() };
-	Matrix< png::uint_16 > outputMatrixB{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > outputMatrixR{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > outputMatrixG{ inputImage.get_height(), inputImage.get_width() };
+	Matrix< double > outputMatrixB{ inputImage.get_height(), inputImage.get_width() };
+
+	Matrix< double > gaussBlurKernel{ 3, 3 };
+	gaussBlurKernel.set(0, 0, 1.0);
+	gaussBlurKernel.set(0, 1, 2.0);
+	gaussBlurKernel.set(0, 2, 1.0);
+	gaussBlurKernel.set(1, 0, 2.0);
+	gaussBlurKernel.set(1, 1, 4.0);
+	gaussBlurKernel.set(1, 2, 2.0);
+	gaussBlurKernel.set(2, 0, 1.0);
+	gaussBlurKernel.set(2, 1, 2.0);
+	gaussBlurKernel.set(2, 2, 1.0);
+	gaussBlurKernel = (1 / 16.0) * gaussBlurKernel;
+
+	Matrix< double > identityKernel{ 3, 3 };
+	identityKernel.set(0, 0, 0.0);
+	identityKernel.set(0, 1, 0.0);
+	identityKernel.set(0, 2, 0.0);
+	identityKernel.set(1, 0, 0.0);
+	identityKernel.set(1, 1, 1.0);
+	identityKernel.set(1, 2, 0.0);
+	identityKernel.set(2, 0, 0.0);
+	identityKernel.set(2, 1, 0.0);
+	identityKernel.set(2, 2, 0.0);
+
+	for (unsigned int y = 0; y < inputImage.get_height(); y++) {
+		for (unsigned int x = 0; x < inputImage.get_width(); x++) {
+			outputMatrixR.set(y, x, identityKernel.convolution(inputMatrixR, y, x));
+			outputMatrixG.set(y, x, identityKernel.convolution(inputMatrixG, y, x));
+			outputMatrixB.set(y, x, identityKernel.convolution(inputMatrixB, y, x));
+		}
+	}
 
 	// load matrix onto image
 	png::image< png::rgb_pixel_16 > outputImage{ inputImage.get_width(), inputImage.get_height() };
 	for (unsigned int y = 0; y < inputImage.get_height(); y++) {
 		for (unsigned int x = 0; x < inputImage.get_width(); x++) {
-			outputImage[y][x] = png::rgb_pixel_16(outputMatrixR.get(y, x), outputMatrixG.get(y, x), outputMatrixB.get(y, x));
+			outputImage[y][x] = png::rgb_pixel_16(static_cast< png::uint_16 >(outputMatrixR.get(y, x)), static_cast< png::uint_16 >(outputMatrixG.get(y, x)), static_cast< png::uint_16 >(outputMatrixB.get(y, x)));
 		}
 	}
+
 	outputImage.write("images/output/output.png");
 	return 0;
 }
--- KernelImageProcessor/src/math/Matrix.h
+++ KernelImageProcessor/src/math/Matrix.h
@@ -1,9 +1,9 @@
+#pragma once
+
 template <typename T>
 class Matrix {
-private:
-	std::vector<std::vector<T>> matrix;
-
 public:
+	std::vector<std::vector<T>> matrix;
 	Matrix(unsigned int x, unsigned int y) {
 		matrix.resize(x);
 		for (size_t i = 0; i < x; i++) {
@@ -18,4 +18,50 @@ public:
 	T get(unsigned int x, unsigned int y) {
 		return matrix[x][y];
 	}
+
+
+	template <typename T2>
+	Matrix<T> operator*(T2 const& scalar) const {
+		Matrix<T> temp = *this;
+		for (unsigned int y = 0; y < temp.size(); y++) {
+			for (unsigned int x = 0; x < temp[y].size(); x++) {
+				temp[y][x] = scalar * temp[y][x];
+			}
+		}
+		return temp;
+	}
+
+	T convolution(Matrix<T> const& mat, unsigned int const& cy, unsigned int const& cx) const {
+		double output = 0;
+		for (unsigned int y = cy - matrix.size() / 2; y < cy + matrix.size() / 2; y++) {
+			unsigned int tempy = y;
+			if (y < 0) {
+				tempy = 0;
+			} else if (y >= mat.matrix.size()) {
+				tempy = mat.matrix.size() - 1;
+			}
+			for (unsigned int x = cx - matrix[0].size() / 2; x < cx + matrix[0].size() / 2; x++) {
+				unsigned int tempx = x;
+				if (x < 0) {
+					tempx = 0;
+				} else if (x >= mat.matrix[0].size()) {
+					tempx = mat.matrix[0].size() - 1;
+				}
+
+				output += mat.matrix[tempy][tempx] * matrix[y - (cy - matrix.size() / 2)][ x - (cx - matrix[0].size() / 2)];
+			}
+		}
+		return output;
+	}
 };
+
+template <typename T>
+inline Matrix<T> operator*(T const& scalar, Matrix<T>& m) {
+	Matrix<T> temp = m;
+	for (unsigned int y = 0; y < temp.matrix.size(); y++) {
+		for (unsigned int x = 0; x < temp.matrix[y].size(); x++) {
+			temp.set(y, x, scalar * temp.get(y, x));
+		}
+	}
+	return temp;
+}
